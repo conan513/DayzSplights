5cb5e81ca552e24e8b0e1d08e7bef3499e113326
ConanHUN <conan_513@hotmail.com>
2014-01-29 20:50:49 +0100

    Custom ambient songs

---
+++ SQF/dayz_sfx/CfgMusic.hpp
@@ -0,0 +1,235 @@
+class CfgMusic
+{
+	class PitchWhine
+	{
+		name = "PitchWhine";
+		sound[] = {"\dayz_sfx\effects\pitchWine.ogg", db+20, 1.0};
+	};
+	class dayz_track_death_1
+	{
+		name = "DayZTrackDeath";
+		sound[] = {"\dayz_sfx\tracks\track_death_1.ogg", db+0, 1.0};
+	};
+	class dayz_track_intro_1
+	{
+		name = "DayZTrackIntro";
+		sound[] = {"\dayz_sfx\tracks\track_intro_1.ogg", db+0, 1.0};
+		duration = ;
+	};
+	class z_suspense_0
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_0.ogg", db+0, 1.0};
+		duration = 17;
+	};
+	class z_suspense_1
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_1.ogg", db+0, 1.0};
+		duration = 13;
+	};
+	class z_suspense_2
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_2.ogg", db+0, 1.0};
+		duration = 14;
+	};
+	class z_suspense_3
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_3.ogg", db+0, 1.0};
+		duration = 16;
+	};
+	class z_suspense_4
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_4.ogg", db+0, 1.0};
+		duration = 13;
+	};
+	class z_suspense_5
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_5.ogg", db+0, 1.0};
+		duration = 15;
+	};
+	class z_suspense_6
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_6.ogg", db+0, 1.0};
+		duration = 22;
+	};
+	class z_suspense_7
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_7.ogg", db+0, 1.0};
+		duration = 17;
+	};
+	class z_suspense_8
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_8.ogg", db+0, 1.0};
+		duration = 24;
+	};
+	class z_suspense_9
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_9.ogg", db+0, 1.0};
+		duration = 18;
+	};
+	class z_suspense_10
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_10.ogg", db+0, 1.0};
+		duration = 14;
+	};
+	class z_suspense_11
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_11.ogg", db+0, 1.0};
+		duration = 15;
+	};
+	class z_suspense_12
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_12.ogg", db+0, 1.0};
+		duration = 15;
+	};
+	class z_suspense_13
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_13.ogg", db+0, 1.0};
+		duration = 12;
+	};
+	class z_suspense_14
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_14.ogg", db+0, 1.0};
+		duration = 6;
+	};
+	class z_suspense_15
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_15.ogg", db+0, 1.0};
+		duration = 23;
+	};
+	class z_suspense_16
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_16.ogg", db+0, 1.0};
+		duration = 17;
+	};
+	class z_suspense_17
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_17.ogg", db+0, 1.0};
+		duration = 207;
+	};
+	class z_suspense_18
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_18.ogg", db+0, 1.0};
+		duration = 13;
+	};
+	class z_suspense_19
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_19.ogg", db+0, 1.0};
+		duration = 17;
+	};
+	class z_suspense_20
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_20.ogg", db+0, 1.0};
+		duration = 199;
+	};
+	class z_suspense_21
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_21.ogg", db+0, 1.0};
+		duration = 358;
+	};
+	class z_suspense_22
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_22.ogg", db+0, 1.0};
+		duration = 289;
+	};
+	class z_suspense_23
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_23.ogg", db+0, 1.0};
+		duration = 283;
+	};
+	class z_suspense_24
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_24.ogg", db+0, 1.0};
+		duration = 341;
+	};
+	class z_suspense_25
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_25.ogg", db+0, 1.0};
+		duration = 184;
+	};
+	class z_suspense_26
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_26.ogg", db+0, 1.0};
+		duration = 228;
+	};
+	class z_suspense_27
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_27.ogg", db+0, 1.0};
+		duration = 302;
+	};
+	class z_suspense_28
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_28.ogg", db+0, 1.0};
+		duration = 274;
+	};
+	class z_suspense_29
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_29.ogg", db+0, 1.0};
+		duration = 286;
+	};
+	class z_suspense_30
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_30.ogg", db+0, 1.0};
+		duration = 207;
+	};
+	class z_suspense_31
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_31.ogg", db+0, 1.0};
+		duration = 295;
+	};
+	class z_suspense_32
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_32.ogg", db+0, 1.0};
+		duration = 263;
+	};
+	class z_suspense_33
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_33.ogg", db+0, 1.0};
+		duration = 14;
+	};
+	class z_suspense_34
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_34.ogg", db+0, 1.0};
+		duration = 15;
+	};
+	class z_suspense_35
+	{
+		name = "";
+		sound[] = {"\dayz_sfx\samples\suspense_35.ogg", db+0, 1.0};
+		duration = 20;
+	};
+};
